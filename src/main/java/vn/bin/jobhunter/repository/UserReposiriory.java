385c66c0c7377a7262017c402eac996d0a1b1b41
unknown <biennguyen0304@gmail.com>
2025-07-28 20:18:10 +0700

    #19 - #52. loadUserByUsername

--- src/main/java/vn/bin/jobhunter/repository/UserReposiriory.java
+++ src/main/java/vn/bin/jobhunter/repository/UserReposiriory.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.bin.jobhunter.domain.User;
+import java.util.List;
 
 @Repository
 public interface UserReposiriory extends JpaRepository<User, Long> {
-
+    User findByEmail(String email);
 }
